2677d1d8a97717d962bc916c415b88f2104e5055
vorgehen <robert.risch@vorgehen.de>
2025-10-08 15:53:05 +0200

    Correted routing

--- private/views/fragen.php
+++ private/views/fragen.php
@@ -3,7 +3,7 @@
 	$translation = $_SESSION['translation'];
 ?>
 
-<form action="../app/php/fragen_send.php" method="post" enctype="multipart/form-data">
+<form action="api/fragen_send" method="post" enctype="multipart/form-data">
     <label for="pdfFile">Choose a PDF file:</label>
     <input type="file" name="pdfFile" id="pdfFile" accept=".pdf" required>
     <button type="submit">Upload</button>
